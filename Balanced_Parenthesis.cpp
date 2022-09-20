2721235ebaf821aa8b3587a81ba0981f9dc374fd
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-09-20 18:10:06 +0530

    Update Balanced_Parenthesis.cpp

--- Balanced_Parenthesis.cpp
+++ Balanced_Parenthesis.cpp
@@ -1,29 +1,37 @@
-class Solution {
+#include<bits/stdc++.h>
+using namespace std;
+class Solution 
+{
 public:
     bool isValid(string s) 
     {
-        stack <char> st;
-        for(char itr : s)
+        bool ans=false;
+        stack<char> st;
+        for(int i=0;i<s.length();i++)
         {
-            if(itr=='{' or itr=='[' or itr=='(')
-            {
-                st.push(itr);
-            }
+            if(s[i]=='{' || s[i]=='[' || s[i]=='(')
+                st.push(s[i]);
             else
             {
-                if(  (st.top()=='(' && itr==')' )  || (st.top()=='{' && itr=='}' ) || (st.top()=='[' && itr==']' ) )
-                {
-                    st.pop();
-                }
-                else
+                if(st.empty())
+                    return false;
+                if(s[i]==')' and st.top() != '(' or s[i]==']' and st.top() != '[' or  s[i]=='}' and st.top() != '{')
                 {
                     return false;
                 }
+                    st.pop();
             }
         }
         if(st.empty())
             return true;
-        
-        return false;
+        return ans;;
     }
 };
+
+int main()
+{
+    Solution obj;
+    bool ans=obj.isValid("]");
+    cout<<ans;
+
+}
