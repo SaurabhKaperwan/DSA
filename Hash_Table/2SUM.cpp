ea5209707e5c4ebfa649a2e6cd58b74fc3499c81
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-10-28 10:55:30 +0530

    Create 2SUM.cpp

---
+++ Hash_Table/2SUM.cpp
@@ -0,0 +1,25 @@
+//Hash Table
+class Solution 
+{
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            unordered_map<int,int> ump;
+            vector<int> ans;
+            
+            for(int i=0;i<nums.size();i++)
+            {
+                if(ump.find(target-nums[i]) != ump.end())
+                {
+                    ans.push_back(ump[target-nums[i]]);
+                    ans.push_back(i);
+                    return ans;
+                }
+                else
+                {
+                    ump[nums[i]]=i;
+                }
+            }
+            return ans;
+        }
+};
