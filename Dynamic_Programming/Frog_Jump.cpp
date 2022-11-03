4df2fe5f460a654e8827d679012d4065925a4821
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-11-03 21:43:46 +0530

    Update Frog_Jump.cpp

--- Dynamic_Programming/Frog_Jump.cpp
+++ Dynamic_Programming/Frog_Jump.cpp
@@ -22,3 +22,24 @@ int frogJump(int n, vector<int> &heights)
     vector<int> dp(n+1,-1);
     return min_total(n-1,heights,dp);    
 }
+
+
+
+//Tabulation
+#include <bits/stdc++.h> 
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n+1,-1);
+    dp[0]=0;
+    for(int i=1;i<n;i++)
+    {
+        int l = dp[i-1]+abs(heights[i]-heights[i-1]);
+        int r=INT_MAX;
+        if(i>1)
+            r = dp[i-2]+abs(heights[i]-heights[i-2]);
+        
+        dp[i]=min(l,r);                        
+    }
+    return dp[n-1];
+}
