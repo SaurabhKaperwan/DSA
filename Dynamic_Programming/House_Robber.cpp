223f12fa05994ba44ddb1ca484ac5eb189b129b0
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-11-04 19:57:31 +0530

    Update House_Robber.cpp

--- Dynamic_Programming/House_Robber.cpp
+++ Dynamic_Programming/House_Robber.cpp
@@ -26,3 +26,27 @@ class Solution
             return giveRob(n-1,nums,dp);
         }
 };
+
+
+//Tabulation(Space Optimized)
+class Solution 
+{
+    public:
+        int rob(vector<int>& nums) 
+        {
+            int prev=nums[0];
+            int prev2=0;
+
+            for(int i=1;i<nums.size();i++)
+            {
+                int take=nums[i];
+                if(i>1)
+                    take+=prev2;
+                
+                int notTake=prev;
+                prev2=prev;
+                prev=max(take,notTake);
+            }
+            return prev;
+        }
+};
