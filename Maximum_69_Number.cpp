9d91789ea69e66a59605c21a7d74796bb3022f5b
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-11-07 20:04:05 +0530

    Create Maximum_69_Number.cpp

---
+++ Maximum_69_Number.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public:
+        int maximum69Number (int num) 
+        {
+            int temp=num;
+            int rem;
+            int itr=-1;
+            int digit=0;
+            while(temp!=0)
+            {
+                 digit++;
+                rem=temp%10;
+                if(rem==6)
+                    itr=digit;
+                temp=temp/10;
+               
+            }
+            num=num+3*pow(10,itr-1);
+            return num;
+        }
+};
+
+
