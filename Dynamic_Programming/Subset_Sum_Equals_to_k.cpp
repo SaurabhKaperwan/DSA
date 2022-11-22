a9e69d5c1223676c4a6d687033d4844ae27aebf2
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-11-22 19:40:14 +0530

    Update Subset_Sum_Equals_to_k.cpp

--- Dynamic_Programming/Subset_Sum_Equals_to_k.cpp
+++ Dynamic_Programming/Subset_Sum_Equals_to_k.cpp
@@ -25,3 +25,30 @@ bool subsetSumToK(int n, int k, vector<int> &arr)
     vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
     return giveSubset(arr,k,n-1,dp);
 }
+
+
+//Tabulation
+
+
+bool subsetSumToK(int n, int k, vector<int> &arr) 
+{
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0;i<n;i++)
+        dp[i][0]=true;
+    
+    dp[0][arr[0]]=true;
+    for(int ind=1;ind<n;ind++)
+    {
+        for(int target=1;target<=k;target++)
+        {
+            bool notPick = dp[ind-1][target];
+            bool pick = false;
+            if(arr[ind] <= target)
+                pick = dp[ind-1][target-arr[ind]];
+            
+            dp[ind][target]=pick or notPick;
+        }
+    }
+    return dp[n-1][k];
+}
