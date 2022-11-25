9bb94d9da3c6bb9ad628b3eab0cd738857727e59
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-11-25 19:32:14 +0530

    Update Longest_Common_Subsequence.cpp

--- Dynamic_Programming/Longest_Common_Subsequence.cpp
+++ Dynamic_Programming/Longest_Common_Subsequence.cpp
@@ -25,3 +25,33 @@ class Solution
             return lcsUtil(text1,text2,n-1,m-1,dp);
         }
 };
+
+//Memoization(shifting of index)
+
+//Shifting
+
+class Solution 
+{
+    public:
+
+        int lcsUtil(string  &text1,string &text2,int n,int m,vector<vector<int>> &dp)
+        {
+            if(n==0 or m==0)
+                return 0;
+
+            if(dp[n][m]!=-1)
+                return dp[n][m];
+            if(text1[n-1]==text2[m-1])
+                return dp[n][m]=1+lcsUtil(text1,text2,n-1,m-1,dp);
+            
+            return dp[n][m]= max(lcsUtil(text1,text2,n-1,m,dp),lcsUtil(text1,text2,n,m-1,dp));
+        }
+
+        int longestCommonSubsequence(string text1, string text2) 
+        {
+            int n=text1.size();
+            int m=text2.size();
+            vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+            return lcsUtil(text1,text2,n,m,dp);
+        }
+};
