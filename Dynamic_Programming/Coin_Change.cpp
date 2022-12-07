305b3ff6680d99d125850cf635eb3b05a97093a3
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-12-07 20:22:14 +0530

    Update Coin_Change.cpp

--- Dynamic_Programming/Coin_Change.cpp
+++ Dynamic_Programming/Coin_Change.cpp
@@ -52,34 +52,6 @@ class Solution
 {
     public:
 
-        int hlp(int ind , int amount , vector<int>& coins,vector<vector<int>> &dp)
-        {
-
-            if(dp[ind][amount] != -1)
-                return dp[ind][amount];
-
-            if(ind == 0)
-            {
-                if(amount % coins[ind] == 0 )
-                {
-                    return amount / coins[ind];
-                }
-                else 
-                {
-                    return 1e9;
-                }
-            }
-
-            int notPick = 0 + hlp(ind-1,amount,coins,dp);
-
-            int pick = 1e9;
-
-            if(coins[ind] <= amount)
-                pick = 1 + hlp(ind,amount - coins[ind] , coins,dp);
-
-            return dp[ind][amount] = min(notPick,pick);
-        }
-
         int coinChange(vector<int>& coins, int amount) 
         {
             int n = coins.size();
