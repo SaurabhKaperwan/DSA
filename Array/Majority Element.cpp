53cfd11fbeb0f72a38d2727dd13d64f546c18b00
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2023-03-16 18:31:49 +0530

    Create

---
+++ Array/Majority Element.cpp
@@ -0,0 +1,51 @@
+// Using Map
+
+class Solution 
+{
+    public:
+        int majorityElement(vector<int>& nums) 
+        {
+            unordered_map<int,int> mp;
+            int ans;
+            for(auto num : nums)
+            {
+                mp[num]++;
+            }
+            
+            for(auto itr : mp)
+            {
+                if(itr.second > nums.size()/2)
+                {
+                    ans=itr.first;
+                    break;
+                }
+            }
+            return ans;
+        }
+};
+
+//Using Moore's Voting Algo
+
+class Solution 
+{
+    public:
+        int majorityElement(vector<int>& nums) 
+        {
+            int n = nums.size();
+            int el;
+            int cnt = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(cnt == 0)
+                {
+                    cnt = 1;
+                    el = nums[i];
+                }
+                else if(el == nums[i])
+                    cnt++;
+                else
+                    cnt--;
+            }
+            return el;
+        }
+};
